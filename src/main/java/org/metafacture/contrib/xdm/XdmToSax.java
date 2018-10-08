5764a1a75fe21576c96d6debbd42ceebf02674f7
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-10-08 15:59:34 +0200

    Add contrib sub-package
    Moves everything under org.metafacture into org.metafacture.contrib

---
+++ src/main/java/org/metafacture/contrib/xdm/XdmToSax.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Deutsche Nationalbibliothek
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metafacture.contrib.xdm;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SAXDestination;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmNode;
+import org.metafacture.contrib.framework.XdmReceiver;
+import org.metafacture.contrib.framework.helpers.DefaultXdmPipe;
+import org.metafacture.contrib.xdm.helper.XmlFilterAdapter;
+import org.metafacture.framework.FluxCommand;
+import org.metafacture.framework.MetafactureException;
+import org.metafacture.framework.XmlReceiver;
+import org.metafacture.framework.annotations.Description;
+import org.metafacture.framework.annotations.In;
+import org.metafacture.framework.annotations.Out;
+
+@In(XdmReceiver.class)
+@Out(XmlReceiver.class)
+@Description("Transforms xdm nodes into sax documents.")
+@FluxCommand("xdm-to-sax")
+public class XdmToSax extends DefaultXdmPipe<XmlReceiver> {
+    private Processor processor;
+    private SAXDestination destination;
+
+    public XdmToSax() {
+        this.processor = new Processor(false);
+    }
+
+    @Override
+    public void process(XdmNode node) {
+        try {
+            processor.writeXdmValue(node, destination);
+        } catch (SaxonApiException e) {
+            throw new MetafactureException(e);
+        }
+    }
+
+    @Override
+    public void onSetReceiver() {
+        this.destination = new SAXDestination(new XmlFilterAdapter(getReceiver()));
+    }
+}
---
+++ src/main/java/org/metafacture/contrib/xdm/XdmXmlEncoder.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Deutsche Nationalbibliothek
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metafacture.contrib.xdm;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
+import net.sf.saxon.s9api.XdmNode;
+import org.metafacture.contrib.framework.XdmReceiver;
+import org.metafacture.contrib.framework.helpers.DefaultXdmPipe;
+import org.metafacture.framework.FluxCommand;
+import org.metafacture.framework.MetafactureException;
+import org.metafacture.framework.ObjectReceiver;
+import org.metafacture.framework.annotations.Description;
+import org.metafacture.framework.annotations.In;
+import org.metafacture.framework.annotations.Out;
+
+@In(XdmReceiver.class)
+@Out(String.class)
+@Description("Encodes xdm nodes into their xml equivalent.")
+@FluxCommand("xdm-to-xml")
+public class XdmXmlEncoder extends DefaultXdmPipe<ObjectReceiver<String>> {
+    private Processor processor;
+    private Serializer serializer;
+
+    public XdmXmlEncoder() {
+        this.processor = new Processor(false);
+        this.serializer = processor.newSerializer();
+
+        this.serializer.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "no");
+        this.serializer.setOutputProperty(Serializer.Property.ENCODING, "UTF-8");
+        this.serializer.setOutputProperty(Serializer.Property.METHOD, "xml");
+        this.serializer.setOutputProperty(Serializer.Property.INDENT, "yes");
+    }
+
+    public void setEncoding(String encoding) {
+        serializer.setOutputProperty(Serializer.Property.ENCODING, encoding);
+    }
+
+    public void setOmitDeclaration(boolean omit) {
+        if (omit) {
+            serializer.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "yes");
+        } else {
+            serializer.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "no");
+        }
+    }
+
+    @Override
+    public void process(XdmNode node) {
+        try {
+            String xml = serializer.serializeNodeToString(node);
+            getReceiver().process(xml);
+        } catch (SaxonApiException e) {
+            throw new MetafactureException(e);
+        }
+    }
+}
